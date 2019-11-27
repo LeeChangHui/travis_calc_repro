0c418d4cc6c7d44cb17bd8c44704971c34d06321
PARKHAENGUN <jknm3333@naver.com>
2019-11-27 14:37:08 +0900

    initial commit

---
+++ src/main/java/kr/ac/gnu/anse/calc/Calc.java
@@ -0,0 +1,16 @@
+package kr.ac.gnu.anse.calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calc 
+{
+	public int add(int x, int y) {
+		return x+y;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
